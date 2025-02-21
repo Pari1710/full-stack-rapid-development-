931d5a5ea74c6a9e5761a7c324f5cb52a7e2521f
Pari1710 <parigawali@gmail.com>
2025-02-21 19:38:41 +0530

    added database connnection

---
+++ backend/db/database.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"log"
+	"backend/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := "host=localhost user=postgres password=postgres dbname=tasks port=5432 sslmode=disable"
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	database.AutoMigrate(&models.User{}, &models.Task{})
+	DB = database
+}
